6cd179fb15179fd1ec7958624f32324f61acd495
Irides-Chromium <zhuhaoqing@live.cn>
2017-08-23 12:02:09 -0500

    Fixed environment

--- env.go
+++ env.go
@@ -4,34 +4,32 @@ import (
 
 )
 
-type Scope map[string]Object
-
 type Env struct {
-    scopes    []Scope
+	scope   map[string]Object
+	next    *Env
 }
 
-func NewEnv() (e *Env) {
-    e.Push()
-    return
+func NewEnv(outer *Env) *Env {
+    return &Env{make(map[string]Object), outer}
 }
 
-func (e * Env) Push() {
-    // Creates a new scope (pushes a stack frame)
-    // The new scope is the first
-    e.scopes = append([]Scope{make(Scope)}, e.scopes...)
-}
-
-func (e * Env) Pop() {
-    e.scopes = e.scopes[1:]
-}
+//func (e * Env) Push() {
+//    // Creates a new scope (pushes a stack frame)
+//    // The new scope is the first
+//    e.scopes = append([]Scope{make(Scope)}, e.scopes...)
+//}
+//
+//func (e * Env) Pop() {
+//    e.scopes = e.scopes[1:]
+//}
 
 func (e * Env) NewVar(vname string) {  // Creates a new variable in the current scope
-    e.scopes[0][vname] = nil
+    e.scope[vname] = nil
 }
 
 func (e * Env) GetVar(vname string) Object {
-    for _, scope := range e.scopes {
-        if item, ok := scope[vname]; ok {
+    for ; e != nil; e = e.next {
+        if item, ok := e.scope[vname]; ok {
             return item
         }
     }
@@ -39,9 +37,9 @@ func (e * Env) GetVar(vname string) Object {
 }
 
 func (e * Env) SetVar(vname string, val Object) {
-    for _, scope := range e.scopes {
-        if item, ok := scope[vname]; ok {
-            e.scopes[i][vname] = val
+    for ; e != nil; e = e.next {
+        if item, ok := e.scope[vname]; ok {
+            e.scope[vname] = val
             return
         }
     }
@@ -51,13 +49,13 @@ func (e * Env) SetVar(vname string, val Object) {
 func (e * Env) SetVarX(vname string, val Object) {
     // In the current scope, set the var named vname
     // Don't check for variable existence
-    e.scopes[0][vname] = val
+    e.scope[vname] = val
 }
 
-func (e * Env) Freeze() *Env {
-    // Freezes the current environment for function objects
-    frozen := new(Env)
-    frozen.scopes = make([]Scope, len(e.scopes))
-    copy(frozen.scopes, e.scopes)
-    return frozen
-}
+//func (e * Env) Freeze() *Env {
+//    // Freezes the current environment for function objects
+//    frozen := new(Env)
+//    frozen.scopes = make([]Scope, len(e.scopes))
+//    copy(frozen.scopes, e.scopes)
+//    return frozen
+//}
